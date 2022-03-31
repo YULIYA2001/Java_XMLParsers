316fddb7bcb2f03f41c3cdfeb475691efe82ca5e
yuliya <gyuliya2001@gmail.com>
2022-03-31 18:24:34 +0300

    add stax case to menu

--- src/main/java/com/golubovich/xml/XMLMain.java
+++ src/main/java/com/golubovich/xml/XMLMain.java
@@ -2,6 +2,7 @@ package com.golubovich.xml;
 
 import com.golubovich.xml.bean.Flower;
 import com.golubovich.xml.parsers.DOMParser;
+import com.golubovich.xml.parsers.StAXParser;
 import com.golubovich.xml.parsers.saxparser.SAXMyParser;
 import com.golubovich.xml.utils.MyException;
 import java.util.List;
@@ -17,30 +18,26 @@ public class XMLMain {
   public static void main(String[] args) {
     final String DOM = "1";
     final String SAX = "2";
+    final String StAX = "3";
+    final String JAXB = "4";
     final String EXIT = "0";
 
     Scanner in = new Scanner(System.in);
     System.out.println("\t Выберите вид парсера\n-------------------------");
 
     while (true) {
-      List<Flower> flowersList;
-      System.out.println("1. DOM \t 2. SAX \t 0. Выход");
+      List<Flower> flowersList = null;
+      System.out.println("\n1. DOM \t 2. SAX \t 3. StAX \t 4. JAXB \t 0. Выход");
 
       switch (in.nextLine()) {
         case DOM:
           DOMParser domParser = new DOMParser();
-
           try {
             flowersList = domParser.parse();
           } catch (MyException e) {
             logger.error(e.getMessage());
             return;
           }
-
-          for (Flower flower: flowersList) {
-            logger.info(flower);
-          }
-
           break;
 
         case SAX:
@@ -51,11 +48,26 @@ public class XMLMain {
             logger.error(e.getMessage());
             return;
           }
+          break;
 
-          for (Flower flower: flowersList) {
-            logger.info(flower);
+        case StAX:
+          StAXParser staxParser = new StAXParser();
+          try {
+            flowersList = staxParser.parse();
+          } catch (MyException e) {
+            logger.error(e.getMessage());
+            return;
           }
+          break;
 
+        case JAXB:
+//          JAXBParser jaxbParser = new JAXBParser();
+//          try {
+//            flowersList = jaxbParser.parse();
+//          } catch (MyException e) {
+//            logger.error(e.getMessage());
+//            return;
+//          }
           break;
 
         case EXIT:
@@ -63,6 +75,13 @@ public class XMLMain {
         default:
           System.out.println("Неверный ввод. Попробуйте снова");
       }
+
+      if (flowersList != null) {
+        for (Flower flower : flowersList) {
+          logger.info(flower);
+        }
+      }
+
     }
   }
 }
